815c9234c7ed44a46ea8274b6b0c0c374e27c2b6
holarxmanuel <113714187+holarxmanuel@users.noreply.github.com>
2024-09-03 04:53:39 -0400

    Added the zip_code fieldname with default 11111

--- store/models.py
+++ store/models.py
@@ -64,6 +64,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip_code = models.IntegerField(default=11111)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE) 
 
 
